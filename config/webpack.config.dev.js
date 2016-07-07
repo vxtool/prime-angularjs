9cc22986666e5f9154007002d365d3f773ce6ff6
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-07-07 05:35:35 -0300

    add: webpack config dev

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,70 @@
+var path = require('path');
+var webpack = require('webpack');
+
+// App files location
+var PATHS = {
+  app: path.resolve(__dirname, '../source/js'),
+  build: path.resolve(__dirname, '../dist')
+};
+
+var plugins = [
+  // Avoid publishing files when compilation fails
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('development'),
+    __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+  }),
+  new webpack.optimize.OccurenceOrderPlugin()
+];
+
+var modulesDirectories = [
+  '../node_modules'
+];
+
+var aliases = {
+  'angular': path.resolve(__dirname, '../node_modules/angular/')
+};
+
+module.exports = {
+  env : process.env.NODE_ENV,
+  entry: {
+    app: path.resolve(PATHS.app, 'app.js'),
+    vendor: [
+      'angular'
+    ]
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'js/scripts.min.js',
+    publicPath: '/'
+  },
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  resolve: {
+    extensions: ['', '.js'],
+    modulesDirectories: modulesDirectories,
+    alias: aliases
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0']
+        },
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: plugins,
+  devServer: {
+    contentBase: path.resolve(__dirname, PATHS.build),
+    port: 8000,
+    historyApiFallback: true
+  },
+  devtool: 'eval'
+};
