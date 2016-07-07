36d44cacbff869e8db970f61335bfa3870033e93
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-07-07 03:44:29 -0300

    add: Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+  var path = require('path');
+
+  require('load-grunt-config')(grunt, {
+    init: true,
+    configPath: path.join(process.cwd(), 'tasks/gruntjs'),
+    data: {
+    	projectDev : 'source/',
+    	projectDir : 'dist/',
+      pkg: grunt.file.readJSON('package.json')
+    }
+  });
+};
